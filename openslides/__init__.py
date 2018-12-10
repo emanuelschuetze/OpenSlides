4a19cb670b0e2155307646c36d4762087a754408
Emanuel Schütze <emanuel@intevation.de>
2018-12-10 11:14:10 +0100

    CUSTOM: 3.0-dgb-20181210

--- openslides/__init__.py
+++ openslides/__init__.py
@@ -1,7 +1,7 @@
-__author__ = "OpenSlides Team <support@openslides.org>"
-__description__ = "Presentation and assembly system"
-__version__ = "3.0-dev"
-__license__ = "MIT"
-__url__ = "https://openslides.org"
+__author__ = 'OpenSlides Team <support@openslides.org>'
+__description__ = 'Presentation and assembly system'
+__version__ = '3.0-dgb-20190120'
+__license__ = 'MIT'
+__url__ = 'https://openslides.org'
 
 args = None
